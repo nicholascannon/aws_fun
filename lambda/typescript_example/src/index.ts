5ed816ae44d869b2235cc9116888cb746b6e9c24
Nicholas Cannon <nicholascannon1@gmail.com>
2021-04-28 18:38:38 +0800

    finished lambda

--- lambda/typescript_example/src/index.ts
+++ lambda/typescript_example/src/index.ts
@@ -1,1 +1,20 @@
-// handler code here...
+import { Handler } from "aws-lambda";
+
+interface Event {
+  key1: string;
+  key2: string;
+  key3: string;
+}
+
+interface Response {
+  message: string;
+}
+
+export const handler: Handler<Event, Response> = async (event, context) => {
+  console.log(context);
+  console.log(event);
+
+  return {
+    message: 'Hello from typescript lambda!'
+  };
+};
