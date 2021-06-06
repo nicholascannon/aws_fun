92a89f598472f1c7b3bd7478ad8512aad0cc1ecb
Nicholas Cannon <nicholascannon1@gmail.com>
2021-06-06 11:14:48 +0800

    serverless framework stuff

---
+++ serverless_framework/todoListApi/src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.handler = async (event) => {
+  console.log(`Recieved event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Is running locally: ${process.env.IS_OFFLINE}`);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Serverless Todo list API",
+    }),
+  };
+};
