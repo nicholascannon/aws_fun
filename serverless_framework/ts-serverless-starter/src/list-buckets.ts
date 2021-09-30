ed3c0cd1c61fcc6d030493ef313a71406ce2b67f
Nicholas Cannon <nicholascannon1@gmail.com>
2021-09-30 17:49:22 +0800

    fix typo

--- serverless_framework/ts-serverless-starter/src/list-buckets.ts
+++ serverless_framework/ts-serverless-starter/src/list-buckets.ts
@@ -3,7 +3,7 @@ import S3 from "aws-sdk/clients/s3";
 import { env } from "./env";
 import { Response } from "./shared/response";
 
-const s3Client = new S3({ region: "ap-south-east-2" });
+const s3Client = new S3({ region: "ap-southeast-2" });
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   console.log(`event: ${JSON.stringify(event, null, 2)}`);
